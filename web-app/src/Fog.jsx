43bb9bb00365bf89c00892c71e986c32634aec82
Markham <quinesquines@gmail.com>
2020-05-11 20:58:10 -0700

    Use urls for map; add control for opacity

--- web-app/src/Fog.jsx
+++ web-app/src/Fog.jsx
@@ -1,63 +1,25 @@
-import React from 'react';
-
-class Fog extends React.Component {
-	constructor (props) {
-		super(props);
-		this.state = {};
-	}
-
-	componentDidMount () {
-		this.resetFog();
-		this.props.setCursor(0,0);
-		let self = this;
-		this.getFog().addEventListener('mousemove', evt => {
-			let x = evt.offsetX, y = evt.offsetY;
-			this.props.setCursor(x, y);
-			if (self.state.isErasing) this.fogDot(x, y, 38);
-		})
-		this.getFog().addEventListener('mousedown', evt => {
-			this.setState(Object.assign(self.state, {isErasing: true}));
-			let x = evt.offsetX, y = evt.offsetY;
-			this.props.setCursor(x, y);
-			if (self.state.isErasing) this.fogDot(x, y, 38);
-		})
-		this.getFog().addEventListener('mouseup', evt => {
-			this.setState(Object.assign(self.state, {isErasing: false}));
-		})
-	}
-
-    getFog () { return document.getElementById('canvas-fog') }
-
-    /* Fuzzy erase a circle of fog */
-    fogDot (x, y, r, r2) {
-    	let context = this.getFog().getContext('2d');
-    	context.globalCompositeOperation = 'destination-out';
-    	let grad = context.createRadialGradient(x,y,r2||1, x,y,r*0.75);
-    	grad.addColorStop(0, 'rgba(0,0,0,255)');
-    	grad.addColorStop(1, 'rgba(0,0,0,0)');
-    	context.fillStyle = grad;
-    	context.fillRect(x-r,y-r,x+r,y+r);
-    	context.globalCompositeOperation = 'destination-over';
-    }
-
-    resetFog () {
-    	console.log('setting fog');
-    	let context = this.getFog().getContext('2d');
-    	this.getFog().style.border = '3px dashed red'
-		this.getFog().style.opacity = 0.75;
-    	context.globalCompositeOperation = 'destination-over';
-    	context.fillStyle = 'black';
-    	context.fillRect(0, 0, 9999, 9999);
-    	this.fogDot(111,111,88);
-    	this.fogDot(111,161,88);
-    	this.fogDot(141,111,88);
-    }
-
-    render () {
-    	return (
-    		<canvas id="canvas-fog" />
-    	);
-    }
+/* Fuzzy erase a circle of fog */
+function fogErase (x, y, r, r2) {
+	let context = getFog().getContext('2d');
+	context.globalCompositeOperation = 'destination-out';
+	let grad = context.createRadialGradient(x,y,r2||1, x,y,r*0.75);
+	grad.addColorStop(0, 'rgba(0,0,0,255)');
+	grad.addColorStop(1, 'rgba(0,0,0,0)');
+	context.fillStyle = grad;
+	context.fillRect(x-r,y-r,x+r,y+r);
+	context.globalCompositeOperation = 'destination-over';
 }
-
-export default Fog;
+function getFog () { return document.getElementById('canvas-fog') }
+function resetFog () {
+	let context = getFog().getContext('2d');
+	getFog().style.border = '3px dashed red'
+	getFog().style.opacity = 0.75;
+	context.globalCompositeOperation = 'destination-over';
+	context.fillStyle = 'black';
+	context.fillRect(0, 0, 9999, 9999);
+}
+export const fog = {
+	reset: resetFog,
+	erase: fogErase,
+};
+export default fog;
--- web-app/src/Game.jsx
+++ web-app/src/Game.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Fog from './Fog.jsx';
+import { fog } from './Fog.jsx';
 
 class Game extends React.Component {
 	ws = new WebSocket('ws://localhost:8000/');
@@ -7,16 +7,22 @@ class Game extends React.Component {
 	constructor (props) {
 		super(props);
 		this.isHost = !!props.host;
-		this.resizeCanvases = this.resizeCanvases.bind(this);
-		this.setMap = this.setMap.bind(this);
-		this.setCursor = this.setCursor.bind(this);
-		this.fog = React.createRef();
-		this.selfRef = React.createRef();
-		this.state = {};
+		this.setTool = this.setTool.bind(this);
+		this.drawMap = this.drawMap.bind(this);
+		this.handleCheckbox = this.handleCheckbox.bind(this);
+		this.handleTextField = this.handleTextField.bind(this);
+		this.onMouseUp = this.onMouseUp.bind(this);
+		this.onMouseDown = this.onMouseDown.bind(this);
+		this.onMouseMove = this.onMouseMove.bind(this);
+		this.onClick = this.onClick.bind(this);
+		this.state = {x: -1, y: -1, tool: 'fog',
+		radius: 55,
+		fogOpacity: 0.5,
+		mapUrl: "/FFtri9T.png"};
 	}
 
 	componentDidMount () {
-		/* Define websockets callbacsk */
+		/* Define websockets callbacks */
 		this.ws.onopen = () => {
 			console.log('opened WebSocket');
 		}
@@ -26,13 +32,45 @@ class Game extends React.Component {
 		this.ws.onclose = () => {
 			console.log('closed');
 		}
-		setInterval( _ =>{
-	        this.ws.send( Math.random() )
-	    }, 2000 )
-	    this.resizeCanvases();
-	    /* Define window callbacks */
-	    window.addEventListener('resize', this.resizeCanvases);
-	    /* Define canvas callbacks */
+		setInterval( _ => { this.ws.send( Math.random() ) }, 2000 )
+		/* Define window callbacks */
+		window.addEventListener('resize', this.drawMap);
+		/* Define canvas callbacks */
+		this.mapCanvas = document.getElementById('canvas-map');
+		this.overlay = document.getElementById('overlay');
+		this.overlay.addEventListener('mouseup', this.onMouseUp);
+		this.overlay.addEventListener('mousedown', this.onMouseDown);
+		this.overlay.addEventListener('mousemove', this.onMouseMove);
+		this.drawMap();
+	}
+
+	onMouseMove (evt) {
+console.log('mousemovedd')
+		let x = evt.offsetX, y = evt.offsetY;
+		this.setState({x: x, y: y});
+		if (this.state.tool == 'fog') {
+			this.setPointerOutline();
+			if (this.state.clickOn) fog.erase(x, y, this.state.radius); 
+		}
+	}
+	onMouseUp (evt) { this.setState({ clickOn: false }) }
+	onMouseDown (evt) { this.setState({ clickOn: true }) }
+	onClick (evt) {
+		let x = evt.offsetX, y = evt.offsetY;
+		if (this.state.tool == 'fog') {
+			fog.erase(x, y, this.state.radius); 
+		}
+	}
+	
+	setPointerOutline () {
+		this.overlay.width = this.overlay.width;
+		let ctx = this.overlay.getContext('2d');
+		ctx.strokeStyle = 'yellow'
+		ctx.lineWidth = '2'
+		ctx.beginPath();
+		ctx.arc(this.state.x, this.state.y, this.state.radius, 0, 2*Math.PI);
+		ctx.stroke();
+		ctx.closePath();
 	}
 
 	draw (img, canvasId) {
@@ -40,6 +78,16 @@ class Game extends React.Component {
 		let canvas = document.getElementById(canvasId || 'canvas-map');
 		let context = canvas.getContext('2d');
 		context.drawImage(img, 0, 0);
+	}
+
+	drawMap () {
+		let img = new Image();
+		const ctx = this.mapCanvas.getContext('2d');
+		img.onload = () => {
+			this.resizeCanvases(img.width, img.height);
+			ctx.drawImage(img, 0, 0);
+		}
+		img.src = this.state.mapUrl;
 	}
 
 	getImg (callback) {
@@ -58,45 +106,106 @@ class Game extends React.Component {
 		reader.readAsDataURL(file);
 	}
 
-	render() {
-	    return (
-	    	<div style={{"position": "relative"}}>
-				<canvas id="canvas-map" />
-				<canvas id="canvas-npcs" />
-				<Fog ref={this.selfRef} setCursor={this.setCursor} />
-				<canvas id="canvas-pcs" />
-			    <div id="control-panel">
-					<input type='file' id='file-select' />
-					<button onClick={this.setMap}>Sm</button>
-					<button onClick={this.resetFog}>Rf</button>
-					<span>X {this.state.x} / Y {this.state.y}</span>
-			    </div>
-			</div>
-	    );
-    }
-
-    getFog () { return document.getElementById('canvas-fog') }
-    resetFog () { this.selfRef.current && this.selfRef.current.resetFog() }
-
-    resizeCanvases () {
-    	console.log('resizing canvas');
-    	document.querySelectorAll('canvas').forEach(canvas => {
-    		canvas.width = (this.map && this.map.width) || window.innerWidth;
-    		canvas.height = (this.map && this.map.height) || window.innerHeight;
-    	});
-    	this.resetFog();
-    }
-
-	setMap (evt) {
-		this.getImg(img => {
-			this.map = img;
-			this.resizeCanvases();
-			this.draw(img);
-		});
+	handleTextField (evt) {
+		let obj = {};
+		let key = evt.target.dataset.field;
+		obj[key] = evt.target.value;
+		this.setState(obj);
+		if (key == 'fogOpacity') document.getElementById('canvas-fog').style.opacity = evt.target.value;
+	}
+	handleCheckbox (evt) {
+		let obj = {};
+		let key = evt.target.dataset.field;
+		obj[key] = evt.target.value;
+		this.setState(obj);
 	}
 
-	setCursor (x, y) {
-		this.setState({ x: x, y: y, })
+	render () {
+		return (
+			<div style={{"position": "relative"}}>
+ 				<canvas id="canvas-map" />
+				<canvas id="canvas-npcs" />
+				<canvas id="canvas-fog" className="passthrough" />
+				<canvas id="canvas-pcs" />
+				<canvas id="overlay" />
+				{this.renderControlPanel()}
+			</div>
+		);
+	}
+
+	renderControlPanel () {
+		return (
+			<div id="control-panel">
+				<div>
+					<label>
+						<input type="checkbox" data-field="showCpPcs" onChange={this.handleCheckbox} />
+						Maps...
+					</label>
+					<label>
+						<input type="checkbox" data-field="showCpNpcs" onChange={this.handleCheckbox} />
+						NPCs...
+					</label>
+					<label>
+						<input type="checkbox" data-field="showCpMaps" onChange={this.handleCheckbox} />
+						PCs...
+					</label>
+					<button onClick={fog.reset}>Reset Fog</button>
+					<input onChange={this.handleTextField} data-field="fogOpacity" value={this.state.fogOpacity} size="2" />
+					<label>
+						<input type="radio" name="tool" value="fog" onChange={this.setTool} />
+						fog
+					</label>
+					<label>
+						<input type="radio" name="tool" value="move" onChange={this.setTool} />
+						move
+					</label>
+					<input onChange={this.handleTextField} data-field="radius" value={this.state.radius} size="2" />
+					<span>X {this.state.x} / Y {this.state.y}</span>
+				</div>
+				{this.state.showCpMaps && this.renderCpMaps()}
+				{this.state.showCpNpcs && this.renderCpNpcs()}
+				{this.state.showCpPcs && this.renderCpPcs()}
+			</div>
+		)
+	}
+
+	renderCpMaps () {
+		return (
+			<div id="maps-cp">
+			fds
+			</div>
+		)
+	}
+
+	renderCpNpcs () {
+		return (
+			<div id="npcs-cp">
+			fds
+			</div>
+		)
+	}
+
+	renderCpPcs () {
+		return (
+			<div id="cps-cp">
+			fds
+			</div>
+		)
+	}
+
+	resizeCanvases (w, h) {
+		console.log('resizing canvas');
+		document.querySelectorAll('canvas').forEach(canvas => {
+			canvas.width = w || window.innerWidth;
+			canvas.height = h || window.innerHeight;
+		});
+		fog.reset();
+	}
+
+	setTool (evt) {
+		console.log(evt.target.value, evt.target);
+		this.setState({ tool: evt.target.value });
+		this.overlay.style.zIndex = evt.target.value == 'fog' ? 11 : 0;
 	}
 }
 
