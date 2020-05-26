72be5e333ae49d525883830c198c7f365a14e6f0
Markham <quinesquines@gmail.com>
2020-05-25 18:32:08 -0700

    Fix: can now rename maps

--- web-app/src/cp/Map.jsx
+++ web-app/src/cp/Map.jsx
@@ -48,7 +48,7 @@ class CpMap extends React.Component {
     return (
       <li className={this.klass()}>
         <button onClick={this.editPristine.bind(this)}><span aria-label="edit pristine" title="Edit 'pristine' map" role="img">&#x1f4dd;</span></button>
-        <input type="text" size="2" placeholder="name" onChange={this.handleTextChange.bind(this, 'name')} value={this.map.name|''} />
+        <input type="text" size="2" placeholder="name" onChange={this.handleTextChange.bind(this, 'name')} value={this.map.name||''} />
         <input type="text" size="25" placeholder="url" onChange={this.handleTextChange.bind(this, 'url')} value={this.map.url||''} />
         <button onClick={this.loadSnapshot.bind(this)}><span aria-label="load snapshot" title="Load map snapshot" role="img">&#x25b6;</span></button>
         <button onClick={this.duplicate.bind(this)}><span aria-label="Duplicate map" title="Duplicate map" role="img">&#x1f4cb;</span></button>
